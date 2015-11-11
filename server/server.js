47d28ef4c909554010f1f18441d8c53d64d79b3f
Brian Kwon <brian.y.kwon@gmail.com>
2015-11-11 15:34:21 -0800

    set up server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
-alert("boo");
+var app = require('express');
+var mongoose = require('mongoose');
 
+app = express();
+
+mongoose.connect('mongodb://localhost/cookedin');
+
+require('./config/middleware.js')(app, express);
+
+app.listen(1337);
+
+module.exports = app;
 
 
